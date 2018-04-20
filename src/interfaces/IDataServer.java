5309ed693f025f0167644cde5f448e6a0ee3472c
mskn <mskn@DESKTOP-T0TMA0H.home>
2018-04-20 14:34:09 +0200

    Added validated and execute getStolenParts methods to ILogicServer and IDataServer interfaces

--- src/interfaces/IDataServer.java
+++ src/interfaces/IDataServer.java
@@ -5,8 +5,10 @@ import java.rmi.RemoteException;
 import java.sql.SQLException;
 
 import model.Car;
+import model.PartList;
 
 public interface IDataServer extends Remote
 {
    boolean executeRegisterCar(Car car) throws RemoteException, SQLException;
+   PartList executeGetStolenParts(Car car) throws RemoteException, SQLException;
 }
--- src/interfaces/ILogicServer.java
+++ src/interfaces/ILogicServer.java
@@ -4,8 +4,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import model.Car;
+import model.PartList;
 
 public interface ILogicServer extends Remote
 {
    String validateRegisterCar(Car car) throws RemoteException;
+   PartList validateGetStolenParts(Car car) throws RemoteException;
 }
