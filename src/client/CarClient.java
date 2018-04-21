ccdf60085793714d5acfe3ba8e485ac514b35f28
marek95 <mareksimko95@gmail.com>
2018-04-21 22:47:33 +0200

    mvc for the first car registration client, creation of car object

--- src/client/CarClient.java
+++ src/client/CarClient.java
@@ -5,48 +5,19 @@ import interfaces.ILogicServer;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 
+import client.controller.CarClientController;
+import client.view.CarClientView;
 import model.Car;
 
 public class CarClient
 {
-   private ILogicServer logicServer;
-   
-   public CarClient() throws RemoteException
-   {
-      super();
-   }
-   
-   public void begin()
-   {
-      try {
-         String ip = "localhost";
-         String URL = "rmi://" + ip + "/" + "logicServer";
-         
-         logicServer = (ILogicServer) Naming.lookup( URL );
-         
-         System.out.println("Car Client connection established");
-      } catch( Exception ex ) {
-         ex.printStackTrace();
-      }
-   }
-   
-   public void registerCar(Car car) throws RemoteException
-   {
-      System.out.println(logicServer.validateRegisterCar(car));
-   }
-   
+
    public static void main(String[] args) throws RemoteException
    {
-      CarClient client = new CarClient();
-      
-      client.begin();
-      
-      Car car = new Car();
-      
-      car.setCarId(1);
-      car.setManufacturer("BMW");
-      car.setModel("M3");
-      
-      client.registerCar(car);
+	   CarClientView view =  new CarClientView();
+	   CarClientController controller =  new CarClientController(view);
+	   
+	   controller.begin();
+	   view.start(controller);
    }
 }
---
+++ src/client/controller/CarClientController.java
@@ -0,0 +1,57 @@
+package client.controller;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+import client.view.CarClientView;
+import interfaces.ILogicServer;
+import model.Car;
+
+public class CarClientController {
+
+	private CarClientView view;
+	private ILogicServer logicServer;
+
+	public CarClientController(CarClientView view) throws RemoteException {
+		this.view = view;
+
+	}
+
+	public void execute(String what) throws RemoteException {
+		switch (what) {
+		case "Add Car":
+			Car car = new Car();
+			car.setManufacturer(view.get("Manufacturer"));
+			car.setModel(view.get("Model"));
+			car.setYear(Integer.parseInt(view.get("Year")));
+			car.setWeight(Double.parseDouble(view.get("Weight")));
+			car.setChassisNumber(view.get("Chassis Number"));
+			registerCar(car);
+			break;
+		case "Quit":
+			System.exit(0);
+			break;
+		default:
+			break;
+		}
+	}
+
+	// Network Connection
+	public void begin() {
+		try {
+			String ip = "localhost";
+			String URL = "rmi://" + ip + "/" + "logicServer";
+
+			logicServer = (ILogicServer) Naming.lookup(URL);
+
+			view.show("Car Client connection established");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	// Network Methods
+	public void registerCar(Car car) throws RemoteException {
+		view.show(logicServer.validateRegisterCar(car));
+	}
+}
