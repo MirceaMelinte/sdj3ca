1f3b171e79de869a0146783c3bc45a7784647bf2
marek95 <mareksimko95@gmail.com>
2018-04-22 17:13:38 +0200

    New Methods For Logic Server

--- src/interfaces/ILogicServer.java
+++ src/interfaces/ILogicServer.java
@@ -4,10 +4,28 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import model.Car;
+import model.Pallet;
+import model.Part;
 import model.PartList;
+import model.Product;
 
-public interface ILogicServer extends Remote
-{
-   String validateRegisterCar(Car car) throws RemoteException;
-   PartList validateGetStolenParts(Car car) throws RemoteException;
+public interface ILogicServer extends Remote {
+
+	// client 1
+	String validateRegisterCar(Car car) throws RemoteException;
+
+	// client 2
+	String validateRegisterPart(Part part) throws RemoteException;
+
+	String validateRegisterPallet(Pallet pallet) throws RemoteException;
+
+	String validateFinishPallet(Pallet pallet) throws RemoteException;
+
+	// client 3
+	String validateRegisterProduct(Product product, PartList partList) throws RemoteException;
+
+	PartList getParts() throws RemoteException;
+
+	// client 4
+	PartList validateGetStolenParts(Car car) throws RemoteException;
 }
--- src/logic/LogicServer.java
+++ src/logic/LogicServer.java
@@ -10,80 +10,129 @@ import java.rmi.server.UnicastRemoteObject;
 import java.sql.SQLException;
 
 import model.Car;
+import model.Pallet;
+import model.Part;
 import model.PartList;
+import model.Product;
 
-public class LogicServer extends UnicastRemoteObject implements ILogicServer
-{
-   private static final long serialVersionUID = 1L;
-   private IDataServer dataServer;
-   
-   private PartList cache;
-   
-   public LogicServer() throws RemoteException
-   {
-      super();
-   }
-   
-   public void begin() throws RemoteException
-   {
-      try {       
-         LocateRegistry.getRegistry(1099); // Registry is created on dataServer, here we just get it
-         
-         Naming.rebind("logicServer", this);
-         
-         String ip = "localhost";
-         String URL = "rmi://" + ip + "/" + "dataServer";
-         
-         dataServer = (IDataServer) Naming.lookup( URL );
-         
-         System.out.println("Logic server is running... ");
-         
-         this.cache = this.dataServer.executeGetParts();
-         
-         System.out.println((this.cache != null)
-        		 				? "Parts cache is now up-to-date. "
-	 							: "A problem has occured when updating the parts cache. ");
-         
-      } catch( Exception e ) {
-         e.printStackTrace();
-      }
-   }
-   
-   public static void main(String[] args) throws RemoteException
-   {
-      LogicServer l = new LogicServer();
-      l.begin();
-   }
+public class LogicServer extends UnicastRemoteObject implements ILogicServer {
+	private static final long serialVersionUID = 1L;
+	private IDataServer dataServer;
 
-   @Override
-   public String validateRegisterCar(Car car) throws RemoteException
-   {
-      try
-      {
-         if(dataServer.executeRegisterCar(car) != null)
-         {
-            return "The car was registered";
-         }
-      }
-      catch (SQLException e)
-      {
-         e.printStackTrace();
-      }
-      return "The car registration has failed";
-   }
+	private PartList cache;
 
-   @Override
-   public PartList validateGetStolenParts(Car car) throws RemoteException
-   {
-      try
-      {
-         return dataServer.executeGetStolenParts(car);
-         
-      }
-      catch (SQLException e)
-      {
-         e.printStackTrace();
-      }
-      return null;
-   }
+	public LogicServer() throws RemoteException {
+		super();
+	}
+
+	public void begin() throws RemoteException {
+		try {
+			LocateRegistry.getRegistry(1099); // Registry is created on dataServer, here we just get it
+
+			Naming.rebind("logicServer", this);
+
+			String ip = "localhost";
+			String URL = "rmi://" + ip + "/" + "dataServer";
+
+			dataServer = (IDataServer) Naming.lookup(URL);
+
+			System.out.println("Logic server is running... ");
+
+			this.cache = this.dataServer.executeGetParts();
+
+			System.out.println((this.cache != null) ? "Parts cache is now up-to-date. "
+					: "A problem has occured when updating the parts cache. ");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws RemoteException {
+		LogicServer l = new LogicServer();
+		l.begin();
+	}
+
+	@Override
+	public String validateRegisterCar(Car car) throws RemoteException {
+		try {
+			if (dataServer.executeRegisterCar(car) != null) {
+				return "The car was registered";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "The car registration has failed";
+	}
+
+	@Override
+	public PartList validateGetStolenParts(Car car) throws RemoteException {
+		try {
+			return dataServer.executeGetStolenParts(car);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public String validateRegisterPart(Part part) throws RemoteException {
+		try {
+			Part registeredPart = dataServer.executeRegisterNewPart(part);
+			if (registeredPart != null)
+				return "The part was registered. id: " + registeredPart.getPartId();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "The part registration has failed";
+	}
+
+	@Override
+	public String validateRegisterPallet(Pallet pallet) throws RemoteException {
+		try {
+			Pallet registeredPallet = dataServer.executeRegisterPallet(pallet);
+			if (registeredPallet != null)
+				return "The pallet was registered. id: " + registeredPallet.getPalletId();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "The pallet registration has failed";
+	}
+
+	@Override
+	public String validateRegisterProduct(Product product, PartList partList) throws RemoteException {
+		try {
+			for (Part part : partList.getList()) {
+				dataServer.executeUpdatePartProduct(part, product);
+			}
+			Product registeredProduct = dataServer.executeRegisterProduct(product);
+			if (registeredProduct != null)
+				return "The product was registered. id: " + registeredProduct.getProductId();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "The product registration has failed";
+	}
+
+	@Override
+	public String validateFinishPallet(Pallet pallet) throws RemoteException {
+		try {
+			if (dataServer.executeFinishPallet(pallet) != null)
+				return "Pallet was set as finished";
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "Setting pallet as finished has failed";
+	}
+
+	@Override
+	public PartList getParts() throws RemoteException {
+		try {
+			return dataServer.executeGetParts();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
