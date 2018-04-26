7c1b9485660205dff47e3e58b7809627b0848588
mskn <mskn@DESKTOP-T0TMA0H.home>
2018-04-26 22:53:58 +0200

    Reuploading new model

---
+++ src/model/CarList.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CarList implements Serializable
+{
+   private static final long serialVersionUID = 1L;
+   
+   private List<Car> cars = new ArrayList<Car>();
+   
+   public CarList() {}
+   
+   public void addcar(Car p)
+   {
+      cars.add(p);
+   }
+   
+   public Car removecar(String chassisNumber)
+   {
+      for (int i = 0; i < cars.size(); i++)
+      {
+         if(cars.get(i).getChassisNumber() == chassisNumber)
+         {
+            return cars.remove(i);
+         }
+      }
+      
+      throw new InputMismatchException();
+   }
+   
+   public Car getCar(String chassisNumber)
+   {
+      for (int i = 0; i < cars.size(); i++)
+      {
+         if(cars.get(i).getChassisNumber() == chassisNumber)
+         {
+            return cars.get(i);
+         }
+      }
+      
+      throw new InputMismatchException();
+   }
+   
+   public List<Car> getList()
+   {
+      return cars;
+   }
+   
+   public int count()
+   {
+      return cars.size();
+   }
+   
+   public boolean contains(Car car)
+   {
+      return cars.contains(car);
+   }
+   
+   @Override
+   public String toString()
+   {
+      StringBuilder s = new StringBuilder();
+      
+      for (Car car : cars)
+      {
+         s.append(car.toString() + "\n");
+      }
+      
+      return s.toString();
+   }
+}
