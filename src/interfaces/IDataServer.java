4c349a43a6321f66b2991722de2bbc7337647b45
mskn <mskn@DESKTOP-T0TMA0H.home>
2018-04-27 12:47:41 +0200

    ILogicServer and IDataServer return type for PartCache changed

--- src/interfaces/IDataServer.java
+++ src/interfaces/IDataServer.java
@@ -6,30 +6,14 @@ import java.sql.SQLException;
 
 import model.*;
 
-public interface IDataServer extends Remote {
-	PartList executeGetParts() throws RemoteException, SQLException;
-	
-	CarList executeGetCars() throws RemoteException, SQLException;
-	
-	PalletList executeGetPallets() throws RemoteException, SQLException;
-	
-	ProductList executeGetProducts() throws RemoteException, SQLException;
-
+public interface IDataServer extends Remote 
+{
+	PartCache executeGetParts() throws RemoteException, SQLException;
 	Car executeRegisterCar(Car car) throws RemoteException, SQLException;
-
 	Pallet executeRegisterPallet(Pallet pallet) throws RemoteException, SQLException;
-
 	Product executeRegisterProduct(Product product) throws RemoteException, SQLException;
-
 	Part executeRegisterNewPart(Part part) throws RemoteException, SQLException;
-
 	Part executeUpdatePartPallet(Part part, Pallet pallet) throws RemoteException, SQLException;
-
 	Part executeUpdatePartProduct(Part part, Product product) throws RemoteException, SQLException;
-
-	Pallet executeFinishPallet(Pallet pallet) throws RemoteException, SQLException;
-
-	Car executeFinishCar(Car car) throws RemoteException, SQLException;
-
 	PartList executeGetStolenParts(Car car) throws RemoteException, SQLException;
 }
--- src/interfaces/ILogicServer.java
+++ src/interfaces/ILogicServer.java
@@ -6,6 +6,7 @@ import java.rmi.RemoteException;
 import model.Car;
 import model.Pallet;
 import model.Part;
+import model.PartCache;
 import model.PartList;
 import model.Product;
 
@@ -13,18 +14,13 @@ public interface ILogicServer extends Remote {
 
 	// client 1
 	String validateRegisterCar(Car car) throws RemoteException;
-
 	// client 2
 	String validateRegisterPart(Part part) throws RemoteException;
-
 	String validateRegisterPallet(Pallet pallet) throws RemoteException;
-
-	String validateFinishPallet(Pallet pallet) throws RemoteException;
 
 	// client 3
 	String validateRegisterProduct(Product product, PartList partList) throws RemoteException;
-
-	PartList getParts() throws RemoteException;
+	PartCache getParts() throws RemoteException;
 
 	// client 4
 	PartList validateGetStolenParts(Car car) throws RemoteException;
