e15cb4f1adda798fa0ebd8451d45b43bca1fc862
marek95 <mareksimko95@gmail.com>
2018-04-29 17:54:14 +0200

    final state strings in model

--- src/model/Car.java
+++ src/model/Car.java
@@ -9,58 +9,90 @@ import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(propOrder={"chassisNumber", "manufacturer", "model", "year", "weight", "state"})
+@XmlType(propOrder = { "chassisNumber", "manufacturer", "model", "year", "weight", "state" })
 public class Car implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private String chassisNumber, manufacturer, model, state;
 	private int year;
 	private double weight;
-	
-	public Car() {}
-	
-	public Car(String chassisNumber, String manufacturer,  
-	      String model, int year, double weight, String state)
-	{
-	   this.chassisNumber = chassisNumber;
-	   this.manufacturer = manufacturer;
-	   this.model = model;
-	   this.year = year;
-	   this.weight = weight;
-	   this.state = state;
+
+	public final static String AVAILABLE = "Available";
+	public final static String IN_PROGRESS = "In progress";
+	public final static String FINISHED = "Finished";
+
+	public Car() {
 	}
-	
-   public String getChassisNumber() { return chassisNumber; }
-   public void setChassisNumber(String chassisNumber) { this.chassisNumber = chassisNumber; }
-   
-   public String getManufacturer() { return manufacturer; }
-   public void setManufacturer(String manufacturer) { this.manufacturer = manufacturer; }
-   
-   public String getModel() { return model; }
-   public void setModel(String model) { this.model = model; }
-   
-	public int getYear() { return year; }
-	public void setYear(int year) { this.year = year; }
-	
-	public double getWeight() { return weight; }
-	public void setWeight(double weight) { this.weight = weight; }
-	
-	public String getState() { return state; }
-	public void setState(String state) { this.state = state; }
-	
+
+	public Car(String chassisNumber, String manufacturer, String model, int year, double weight, String state) {
+		this.chassisNumber = chassisNumber;
+		this.manufacturer = manufacturer;
+		this.model = model;
+		this.year = year;
+		this.weight = weight;
+		this.state = state;
+	}
+
+	public String getChassisNumber() {
+		return chassisNumber;
+	}
+
+	public void setChassisNumber(String chassisNumber) {
+		this.chassisNumber = chassisNumber;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
 	@Override
-	public String toString()
-	{
-	   StringBuilder s = new StringBuilder();
-      
-	   s.append("Chassis number: " + chassisNumber);
-      s.append(", Manufacturer: " + manufacturer);
-      s.append(", Model: " + model);
-      s.append(", Year: " + year);
-      s.append(", Weight: " + weight);
-      s.append(", State: " + state);
-      
-      return s.toString();
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+
+		s.append("Chassis number: " + chassisNumber);
+		s.append(", Manufacturer: " + manufacturer);
+		s.append(", Model: " + model);
+		s.append(", Year: " + year);
+		s.append(", Weight: " + weight);
+		s.append(", State: " + state);
+
+		return s.toString();
 	}
 }
