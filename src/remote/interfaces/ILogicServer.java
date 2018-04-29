76cf67c5642132fbdf2f84b01dc4ad486e75fc6a
marek95 <mareksimko95@gmail.com>
2018-04-29 18:03:39 +0200

    new logic server interface ( changes in 2nd client )

--- src/remote/interfaces/ILogicServer.java
+++ src/remote/interfaces/ILogicServer.java
@@ -3,31 +3,41 @@ package remote.interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-import model.Car;
-import model.Pallet;
-import model.Part;
-import model.PartList;
-import model.Product;
-import model.ProductList;
-import model.cache.PartCache;
+import model.*;
+import model.cache.*;
 
 public interface ILogicServer extends Remote {
 
 	// client 1
+	
 	String validateRegisterCar(Car car) throws RemoteException;
+
+	
 	// client 2
-	String validateFinishDismantling(Car car) throws RemoteException; // TODO Client
-	String validateFinishPallet(Pallet pallet) throws RemoteException; // TODO Client
+	
+	CarList getAvailableCars() throws RemoteException;
+
 	String validateRegisterPart(Part part) throws RemoteException;
-	String validateRegisterPallet(Pallet pallet) throws RemoteException;
-	String validateSetPalletWeight(Pallet pallet, double newWeight) throws RemoteException; // TODO Client
+
+	String validatePutPart(int partId, int palletId) throws RemoteException;
+
+	String validateFinishPallet(int palletId) throws RemoteException;
+
+	String validateFinishDismantling(String chassisNumber) throws RemoteException;
+
 
 	// client 3
+	
 	String validateRegisterProduct(Product product, PartList partList) throws RemoteException;
+
 	PartCache getParts() throws RemoteException;
 
+	
 	// client 4
+	
 	PartList validateGetStolenParts(Car car) throws RemoteException;
+
 	ProductList validateGetStolenProducts(Car car) throws RemoteException;
+
 	Car validateGetStolenCar(String chassisNumber) throws RemoteException;
 }
