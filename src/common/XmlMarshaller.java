9bb036eff1ad6acbc47b6d0e17275a512aa55d86
mskn <mskn@DESKTOP-T0TMA0H.home>
2018-04-30 15:31:24 +0200

    Null pointer exception fix in XmlMarshaller class

--- src/common/XmlMarshaller.java
+++ src/common/XmlMarshaller.java
@@ -8,34 +8,43 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-// Generic helper class for XML/Object and Object/XML serialization
-// Methods are generic so they can be reused on any type of objects
-// in the system as long as a Serialization object is of a corresponging
-// of the object to be serialized
+//Generic helper class for XML/Object and Object/XML marshalling
+//Methods are generic so they can be reused on any type of objects
+//in the system as long as a XmlMarshaller object is of a corresponging
+//class of the object to be serialized
 
 public class XmlMarshaller<T>
 {
    @SuppressWarnings("unchecked")
    public T createObjectFromXMLString(String xml, Class<?> cls) throws JAXBException
    {
-      JAXBContext jaxbContext = JAXBContext.newInstance(cls);
-      Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-      StringReader reader = new StringReader(xml);
-      
-      return (T)jaxbUnmarshaller.unmarshal(reader);
+      if(!(xml == (null)))
+      {
+         JAXBContext jaxbContext = JAXBContext.newInstance(cls);
+         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+         StringReader reader = new StringReader(xml);
+         
+         return (T)jaxbUnmarshaller.unmarshal(reader);
+      }
+      return null;
    }   
    
-   // First parameter is the object to be serialized and the second parameter
-   // is the class type of the object to be serialized e.g. PartList.class
+   // First parameter is the object to be marshalled and the second parameter
+   // is the class type of the object to be marshalled e.g. PartList.class
    public String createXMLString(T obj, Class<?> cls) throws JAXBException
    {
-      JAXBContext jaxbContext = JAXBContext.newInstance(cls);
-      Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-      jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);       
-      StringWriter sw = new StringWriter();
-      jaxbMarshaller.marshal(obj, sw);
-      String xmlString = sw.toString();    
-         
-      return xmlString;
+      if(!(obj == (null)))
+      {
+         JAXBContext jaxbContext = JAXBContext.newInstance(cls);
+         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);       
+         StringWriter sw = new StringWriter();
+         jaxbMarshaller.marshal(obj, sw);
+         String xmlString = sw.toString();    
+            
+         return xmlString;
+      }
+      
+      return null;
    }   
 }
