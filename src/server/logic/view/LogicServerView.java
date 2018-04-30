8f1fac5fbe9937b9022f21422dbde434d1b9a986
marek95 <mareksimko95@gmail.com>
2018-04-30 20:12:32 +0200

    minimal mvc for logic server

---
+++ src/server/logic/view/LogicServerView.java
@@ -0,0 +1,19 @@
+package server.logic.view;
+
+import java.util.Scanner;
+
+public class LogicServerView {
+
+	private Scanner in = new Scanner(System.in);
+
+	public void show(String value) {
+		System.out.println(value);
+	}
+	
+	public String get(String what) {
+		System.out.print("Please enter " + what + ": ");
+		String input = in.nextLine();
+		return input;
+	}
+	
+}
