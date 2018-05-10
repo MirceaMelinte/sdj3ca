3140a02498ebaf3dd077fb1a04b52ba35eeb3b9b
mskn <mskn@DESKTOP-T0TMA0H.home>
2018-05-10 21:49:37 +0200

    Redeploying web service after refactoring LogicServerController methods using that use the web service

--- src/webservice/ICarService.java
+++
@@ -1,8 +0,0 @@
-package webservice;
-
-public interface ICarService
-{
-   String traceStolenParts(String chassisNumber);
-   String traceStolenProducts(String chassisNumber);
-   String traceStolenCar(String chassisNumber);
-}
